1f31a2d9d9a96297fbed3196908a66331003c605
Shubham <shubham.marathia@gmail.com>
2018-05-31 01:57:50 +0530

    adding delete script

---
+++ mong-delete.js
@@ -0,0 +1,17 @@
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017' , {useNewUrlParser : true},(err,client)=>{
+    if(err){
+        console.log(err);
+    }
+    const db = client.db('TodoApp');
+    console.log('Database connected at 27017');
+
+    db.collection('Todos').findOneAndDelete({completed: true}).then((res)=>{
+        console.log(res);
+    })
+    client.close();
+});
+
+//deleteone
+//deleteMany
