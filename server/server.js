2973e83524df70a00e05cd53f5fcd4452ef723fb
Shubham <shubham.marathia@gmail.com>
2018-06-05 00:39:03 +0530

    Get todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose-connect');
 var {Todo} = require('./models/todo');
@@ -20,6 +21,35 @@ app.post('/todos' , (req,res)=>{
     },(err)=>{
         res.status(400).send(err);
     });
+});
+
+app.get('/todos' , (req , res) =>{
+    Todo.find().then((doc)=>{
+        res.send({doc});
+    },(err)=>{
+        res.status(400).send(err);
+    });
+})
+
+app.get('/todos/:id' , (req , res) =>{
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)){
+        res.sendStatus(404).send('Id is invalid');
+    }
+    else{
+        Todo.findById(id).then((doc)=>{
+            if(!doc){
+                res.status(404).send('Id not found');
+            }
+            else{
+                res.send(doc);
+            }
+        },(e)=>{
+            res.status(400).send(e);
+        })
+    }
+    // res.send(req.params);
 });
 
 app.listen(3000 , ()=>{
